446e268280288f9b9d351a0c1104a92ac3dda899
Manikuutty Babu <manikuutty.babu@experionglobal.local>
2023-02-14 16:15:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace reversing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr, rev;
+            myStr = "Manikutty";
+            rev = "";
+            Console.WriteLine("String is {0}", myStr);
+            int len;
+            len = myStr.Length - 1;
+            while (len >= 0)
+            {
+                rev = rev + myStr[len];
+                len--;
+            }
+            Console.WriteLine("Reversed String is {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
+
